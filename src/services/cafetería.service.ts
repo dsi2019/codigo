43d328ea73b41d9783a6bf9e0760fd004ccfeae5
Sidd Srinivasan <siddsrinivasan@sidds-mbp.lan>
2019-04-06 17:53:08 +0200

    sample cafeterías

---
+++ src/services/cafetería.service.ts
@@ -0,0 +1,25 @@
+import​ { ​Injectable​ } ​from​ ​"@angular/core"​;
+import​ { ​AngularFireDatabase​ } ​from​ ​"angularfire2/database"​;
+import { Cafetería } from "../models/cafeteríamodel";
+import { Time} from "@angular/common";
+
+@​Injectable​()
+export class CafeteríaService{
+
+    private cafeterías: Cafetería[] = [
+        {
+        nombre: "Sabatini",
+        campus: "Leganes",
+        hora_de_abrir: {hours:9,minutes:0},
+        hora_de_cerrar: {hours:17,minutes:30}
+        },
+        {
+        nombre: "Padre Soler",
+        campus: "Leganes",
+        hora_de_abrir: {hours:9,minutes:0},
+        hora_de_cerrar: {hours:17,minutes:30}
+        },
+
+    ]
+
+}
